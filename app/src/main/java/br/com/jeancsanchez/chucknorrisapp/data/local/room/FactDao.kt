63c9bcfe328f7e9575b743064704a4cb4caf9a66
jeancarlos <jeancs.patricio@gmail.com>
2021-05-08 14:16:01 -0300

    Fixing inheritance of FactDao.kt

--- app/src/main/java/br/com/jeancsanchez/chucknorrisapp/data/local/room/FactDao.kt
+++ app/src/main/java/br/com/jeancsanchez/chucknorrisapp/data/local/room/FactDao.kt
@@ -3,11 +3,10 @@ package br.com.jeancsanchez.chucknorrisapp.data.local.room
 import androidx.room.Dao
 import androidx.room.Query
 import br.com.jeancsanchez.chucknorrisapp.data.dto.ResultDTO
-import br.com.jeancsanchez.chucknorrisapp.data.local.LocalDataSource
 
 @Dao
-interface FactDao : LocalDataSource {
+interface FactDao {
 
     @Query("SELECT * FROM fact WHERE value LIKE :query")
-    override fun findByQuery(query: String): ResultDTO
+    fun findByQuery(query: String): ResultDTO
 }
